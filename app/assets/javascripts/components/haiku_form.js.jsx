ca87d02c9f587664d73e1a05d9dd5c3cc56cb4e5
kangkyu <kangkyu1111@gmail.com>
2015-11-02 23:01:57 -0800

    commit 11-02-15

--- app/assets/javascripts/components/haiku_form.js.jsx
+++ app/assets/javascripts/components/haiku_form.js.jsx
@@ -1,12 +1,53 @@
 var HaikuForm = React.createClass({
+  getInitialState() {
+      return {
+          error:false
+      };
+  },
+  handleSubmit: function(e) {
+    // console.log(e);
+    e.preventDefault();
+  },
 
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log(nextState);
+    var hasErrorChange = nextState.error !== this.state.error;
+    var hasLineChanged = nextState.line !== this.state.line;
+    return hasLineChanged || hasErrorChange;
+  },
+
+  haikuChanged: function(e) {
+    
+    this.setState({line: e.target.value});
+    
+    if (e.target.value === "test") {
+      this.setState({error: true})
+    } else {
+      this.setState({error: false})
+    }
+  },
+
+  _testFunc: function() {
+    if (this.state.error) {
+      return (<p>error</p>);
+    } else {
+      return "";
+    }
+
+  },
 
   render: function() {
+    console.log('Rendering!!');
     return (
+      <div>
+      <h3>{this.state.line}</h3>
+
     <form className="haikuForm" onSubmit={this.handleSubmit}>
-      <input type="text" placeholder="First Haiku Line" />
+      <input type="text" placeholder="First Haiku Line" onChange={this.haikuChanged} />
       <input type="submit" value="Create Haiku!" />
+      {this._testFunc()}
     </form>
+    </div>
     );
   }
 
